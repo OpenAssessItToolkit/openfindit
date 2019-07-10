9725a0c461f98d349e9051fd1d80d9c54113918f
Joel Crawford-Smith <jhc36@duke.edu>
2019-07-10 10:48:12 -0400

    Add a nice demo file

--- run.sh
+++ run.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-if [ -f "docs/assets/alice_prev.csv" ]; then
+if [ -f "example/alice_prev.csv" ]; then
     echo "(bash) - FYI: You have an old report we don't need anymore. Continuing..."
     echo "(bash) - ACTION: Removing old alice_prev.csv report."
-    rm 'docs/assets/alice_prev.csv'
+    rm 'example/alice_prev.csv'
 fi
 
-if [ -f "docs/assets/alice_today.csv" ]; then
+if [ -f "example/alice_today.csv" ]; then
     echo "(bash) - ACTION: Converting alice_today.csv report to be the previous alice_prev.csv report."
-    mv 'docs/assets/alice_today.csv' 'docs/assets/alice_prev.csv';
+    mv 'example/alice_today.csv' 'example/alice_prev.csv';
 fi
 
 echo "(bash) - ACTION: Crawling site to automatically create a current report for today."
-scrapy crawl findfiles -a urls='http://joelcrawfordsmith.com/openassessit/demo/test-pdf-links.html' -s DEPTH_LIMIT=1 -o 'docs/assets/alice_today.csv'
+scrapy crawl findfiles -a urls='http://joelcrawfordsmith.com/openassessit/demo/test-pdf-links.html' --nolog -s DEPTH_LIMIT=1 -o 'example/alice_today.csv'
 echo "(bash) - ACTION: Creating unique hashes for files found in todays new alice_today.csv report."
 python3 ../opendiffit/opendiffit/add_hash.py \
---input-file='docs/assets/alice_today.csv' \
+--input-file='example/alice_today.csv' \
 --output-file='-';
 
-if [ -f "docs/assets/alice_prev.csv" ] && [ -f "docs/assets/alice_prev.csv" ]; then
+if [ -f "example/alice_prev.csv" ] && [ -f "example/alice_prev.csv" ]; then
     echo "(bash) - ACTION: Comparing files and create/update diff column."
     python3 ../opendiffit/opendiffit/identify_diffs.py \
-    --new='docs/assets/alice_today.csv' \
-    --old='docs/assets/alice_prev.csv' \
+    --new='example/alice_today.csv' \
+    --old='example/alice_prev.csv' \
     --diff='-';
     echo "(bash) - HUMAN: Go manually check alice_today.csv 'diff' column for NEW or UPDATED files. Test them for compliance and update the 'comply' column."
     echo "(bash) - Human: Then rerun bash.sh next time you want to check for new or modified PDF files."
