a710a641569651290f8fd54e145dcd018645f590
Joel Crawford-Smith <jhc36@duke.edu>
2020-01-09 08:53:32 -0500

    dev sprint

--- openfindit/spiders/findvideos.py
+++ openfindit/spiders/findvideos.py
@@ -34,41 +34,62 @@ class FindVideosSpider(scrapy.Spider):
 
     def parse(self, response):
         """ Parse all <a>. yield PDF to csv, if not, crawl it  """
-        for a_tag in response.xpath('//a[@href]'):
+        try:
+            for a_tag in response.xpath('//a[@href]'):
 
-            url = response.urljoin(a_tag.attrib['href'])
+                url = response.urljoin(a_tag.attrib['href'])
 
-            if urlparse(url).scheme in ('http', 'https'):
-                request = scrapy.Request(
-                    url, 
-                    callback = self.parse_iframe,
-                    meta=response.meta, 
-                )
-                yield request
+                if urlparse(url).scheme in ('http', 'https'):
+                    request = scrapy.Request(
+                        url,
+                        callback = self.parse_iframe,
+                        meta=response.meta,
+                    )
+                    yield request
 
-            elif 'http' in urlparse(url).scheme:
+                elif 'http' in urlparse(url).scheme:
 
-                yield scrapy.Request(url, self.parse)
-                
+                    yield scrapy.Request(url, self.parse)
+
+        except Exception as ex:
+            print(ex)
+
 
     def parse_iframe(self, response):
         """ Parse all <iframe>. If video yield it to csv """
-        videoUrls = [
-            'https://youtube.com/watch', 
-            'https://www.youtube.com/watch',
-            'https://youtube.com/v', 
-            'https://www.youtube.com/v', 
-            'https://youtu.be/', 
-            'https://youtube.com/embed', 
-            'https://www.youtube.com/embed', 
-            'https://www.youtube-nocookie.com/watch', 
-            'https://youtube-nocookie.com/watch', 
-            'https://www.youtube-nocookie.com/v', 
-            'https://youtube-nocookie.com/v', 
-            'https://player.vimeo', 
-            'https://players.brightcove']
 
-        # Eww, nested function. Fix this.
+        videoUrls = [
+            'https://youtube.com/watch',
+            'https://www.youtube.com/watch',
+            'https://youtube.com/v',
+            'https://www.youtube.com/v',
+            'https://youtu.be/',
+            'https://youtube.com/embed',
+            'https://www.youtube.com/embed',
+            'https://www.youtube-nocookie.com/watch',
+            'https://youtube-nocookie.com/watch',
+            'https://www.youtube-nocookie.com/v',
+            'https://youtube-nocookie.com/v',
+            'https://player.vimeo',
+            'https://players.brightcove',
+            'https://warpwire.duke.edu/w',
+            'http://youtube.com/watch',
+            'http://www.youtube.com/watch',
+            'http://youtube.com/v',
+            'http://www.youtube.com/v',
+            'http://youtu.be/',
+            'http://youtube.com/embed',
+            'http://www.youtube.com/embed',
+            'http://www.youtube-nocookie.com/watch',
+            'http://youtube-nocookie.com/watch',
+            'http://www.youtube-nocookie.com/v',
+            'http://youtube-nocookie.com/v',
+            'http://player.vimeo',
+            'http://players.brightcove',
+            'http://warpwire.duke.edu/w']
+
+
+        # TODO: Eww, nested function. Fix this.
         def get_id(url):
             u_pars = urlparse(url)
             quer_v = parse_qs(u_pars.query).get('v')
@@ -82,46 +103,70 @@ class FindVideosSpider(scrapy.Spider):
             href = response.urljoin(a_tag.attrib['href'])
 
             if href.startswith(tuple(videoUrls)):
-                print('Found link to video %s.' % href)
+                print('INFO: Found link to video %s.' % href)
                 # Format YouTube URLs to grab embed url so we can parse it like an iframe
                 if 'youtu' in href:
                     href = "https://youtube.com/embed/" + get_id(href)
-                
+
                 request = scrapy.Request(
-                    href, 
-                    callback = self.parse_video_contents, 
-                    meta=response.meta, 
+                    href,
+                    callback = self.parse_video_contents,
+                    meta=response.meta,
                     dont_filter=True,)
                 yield request
 
         for iframe_tag in response.xpath('//iframe[@src]'):
             src = response.urljoin(iframe_tag.attrib['src'])
-            print('Found iframe... %s' % src)
+            print('INFO: Found iframe... %s' % src)
             if src.startswith(tuple(videoUrls)):
-                print('iframe is a video %s' % src)
+                print('INFO: iframe has a video %s' % src)
                 request = scrapy.Request(
-                    src, 
-                    callback = self.parse_video_contents, 
-                    meta=response.meta, 
+                    src,
+                    callback = self.parse_video_contents,
+                    meta=response.meta,
                     dont_filter=True,)
                 yield request
 
             else:
-                print('iframe not a video.')
+                print('INFO: iframe not a video.')
+
 
     def parse_video_contents(self, response):
+        video_title = "UNKNOWN"
+        video_cc = "UNKNOWN"
+        video_duration = "UNKNOWN"
+
         video_title = response.xpath('//title//text()').extract_first()
         video_title_clean = re.sub(r'\W+', ' ',  video_title)
         video_url_clean = response.url.split('?')[0]
-        video_cc = "UNKNOWN"
-        process = subprocess.Popen(['youtube-dl', '--list-subs', video_url_clean], stdout=subprocess.PIPE)
-        caption_status = str(process.communicate()[0])
-        print(caption_status)
 
-        if "has no subtitles" in caption_status:
-            video_cc = "NO"
-        elif "Available subtitles for" in caption_status:
+        # TODO: Clean this up to only ping once
+        try:
+            subs = subprocess.Popen(['youtube-dl', '--list-subs', video_url_clean], stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+            duration = subprocess.Popen(['youtube-dl', '--get-duration', video_url_clean], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").rstrip()
+
+        except Exception as ex:
+            print(ex)
+
+        if ':' in duration:
+            video_duration = duration
+
+        # subs_status = { "Unsupported URL": "UNKNOWN",
+        #                 "Available subtitles for": "YES",
+        #                 "has no subtitles": "NO",
+        #                 "video doesn't have subtitles": "NO" }
+
+        # video_cc = subs_status.get(subs, "UNKNOWN")
+        # print("subs start")
+        # print(subs)
+        # print("subs end")
+
+        if "Available subtitles for" in subs:
             video_cc = "YES"
+        elif "has no subtitles" in subs:
+            video_cc = "NO"
+        elif "video doesn't have subtitles" in subs:
+            video_cc = "NO"
         else:
             video_cc = "UNKNOWN"
 
@@ -129,6 +174,6 @@ class FindVideosSpider(scrapy.Spider):
             video_url = video_url_clean,
             video_title = video_title_clean,
             from_page_url = response.request.headers.get('Referer'),
-            captioned = video_cc
+            captioned = video_cc,
+            duration = video_duration
         )
-    
