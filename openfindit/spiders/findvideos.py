56cbb9217e271c5a4596b40b9853f301adc3a931
Joel Crawford-Smith <jhc36@duke.edu>
2020-01-17 14:51:31 -0500

    Major refactoring

---
+++ openfindit/spiders/findvideos.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+import sys
+from urllib.parse import urlparse, parse_qs
+import scrapy
+import re
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.utils.httpobj import urlparse
+from ..utils import get_id
+
+class FindFilesSpider(scrapy.Spider):
+    name = 'findvideos'
+
+    def __init__(self, *args, **kwargs):
+        """ Enable urls argument to for start_urls and parse for allowed_domains """
+        urls = kwargs.pop('urls', [])
+        if urls:
+            input = kwargs.get('urls', '').split(',') or []
+            self.start_urls = [urls for d in input]
+            self.allowed_domains = [urlparse(urls).netloc for d in input]
+
+        filename = kwargs.pop('filename', [])
+        if filename:
+            with open(filename) as f:
+                self.start_urls = [url.strip() for url in f.readlines()]
+                self.allowed_domains = [urlparse(url).netloc for url in f.readlines()]
+
+        self.logger.info(self.start_urls)
+        super(FindFilesSpider, self).__init__(*args, **kwargs)
+
+
+    # Constants
+    global VIDEO_URLS
+    global DOCUMENT_EXT
+
+    SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
+    VIDEO_URLS = open(os.path.join(SCRIPT_PATH, '..', 'config', 'findvideos.txt')).read().splitlines()
+    DOCUMENT_EXT = open(os.path.join(SCRIPT_PATH, '..', 'config', 'findfiles.txt')).read().splitlines()
+    
+
+    def parse(self, response):
+        """ Parse all <a>. yield PDF to csv, if not, crawl it  """
+        for iframe_tag in response.xpath('//iframe[@src]'):
+            src = response.urljoin(iframe_tag.attrib['src'])
+            if src.startswith(tuple(VIDEO_URLS)):
+                yield scrapy.Request(
+                    src,
+                    callback = self.parse_video_contents,
+                    meta={'metadata': response.meta, 'video_found_as': 'embed', 'on_page': 'YES', 'video_note': 'Captions probably required.'},
+                    dont_filter=True,)
+
+        for a_tag in response.xpath('//a[@href]'):
+            href = response.urljoin(a_tag.attrib['href'])
+                
+            if href.startswith(tuple(VIDEO_URLS)):
+                if ('youtu' in href) and ('embed' not in href):
+                    href = "https://www.youtube.com/embed/" + get_id(href)
+                yield scrapy.Request(
+                    href,
+                    callback = self._parse_video_contents,
+                    meta={'metadata': response.meta, 'video_found_as': 'link', 'on_page': 'UNKNOWN', 'video_note': 'Captions encouraged on off-site links. Required if overlay.'},
+                    dont_filter=True,)
+            elif href.endswith(tuple(DOCUMENT_EXT)):
+                print('Skip links to documents')
+            else:
+                yield scrapy.Request(href, self.parse)
+
+                    
+    def _parse_video_contents(self, response):
+        """ Inspect video for data """
+        video_title = str(response.xpath('//title//text()').extract_first()) or "UNKNOWN"
+        video_title_clean = re.sub(r'\W+', ' ',  video_title)
+        video_cc = "UNKNOWN"
+        video_duration = "UNKNOWN"
+        video_url = response.url
+        if ('/videoseries' and '/playlist' and 'watch_videos') not in video_url: # If not playlist take off the query string
+            video_url = video_url.split('?')[0]
+        try:
+            print('MY INFO: subprocess placeholder')
+            # TODO: Clean this up to only ping once
+            cc = ""
+            duration = ""
+            cc = subprocess.Popen(['youtube-dl', '--list-subs', '--cache-dir=~/tmp', '--sleep-interval=121', '--max-sleep-interval=131', video_url], stdout=subprocess.PIPE).communicate()[0].decode("utf-8")
+            if '429' in cc:
+                print("WARNING! WARNING! WARNING! WARNING! WARNING! WARNING!")
+                raise CloseSpider("Returned 429 - Too many requests. You're about to get this IP banned. Closing spider.")
+            duration = subprocess.Popen(['youtube-dl', '--get-duration', '--cache-dir=~/tmp', '--sleep-interval=122', '--max-sleep-interval=133', video_url], stdout=subprocess.PIPE).communicate()[0].decode("utf-8").rstrip()
+            if "Available subtitles for" in cc:
+                video_cc = "YES"
+            elif ("has no subtitles" in cc) or ("video doesn't have subtitles" in cc):
+                video_cc = "NO"
+            if ':' in duration:
+                video_duration = duration or "UNKNOWN"
+
+        except Exception as ex:
+            print(ex)
+
+        yield dict(
+            video_url = video_url,
+            video_title = video_title_clean,
+            from_page_url = response.request.headers.get('Referer'),
+            captioned = video_cc,
+            duration = video_duration,
+            location = response.meta['video_found_as'],
+            on_page = response.meta['on_page'],
+            notes = response.meta['video_note']
+        )
