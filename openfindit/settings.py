1d808a5b2c290a802dfc775a4ae9fe20b8b391e2
Joel Crawford-Smith <jhc36@duke.edu>
2021-01-28 13:31:58 -0500

    stop cache of http requests from Scrapy

--- openfindit/settings.py
+++ openfindit/settings.py
@@ -114,7 +114,7 @@ AUTOTHROTTLE_TARGET_CONCURRENCY = 0.5
 
 # Enable and configure HTTP caching (disabled by default)
 # See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-HTTPCACHE_ENABLED = True
+HTTPCACHE_ENABLED = False # Turned off to avoid Box Sync problems
 HTTPCACHE_EXPIRATION_SECS = 43200
 HTTPCACHE_DIR = 'httpcache'
 HTTPCACHE_IGNORE_HTTP_CODES = []
